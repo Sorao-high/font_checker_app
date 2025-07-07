1a8e25fa8a5ac2125c1279f6a5ed9d3254cf1d00
Sorao Kosaka <66772476+Sorao-high@users.noreply.github.com>
2025-07-07 17:56:14 +0900

    Update settings.py

--- font_checker/font_checker/settings.py
+++ font_checker/font_checker/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g6)*(#qjb@b^t23ne)k(a0e09^p^67ppi%pfwt49_2jhd=02q9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
